41347c7f38de1a64316ecc38af677d0523a7a224
Jacob Ellingson <jake.ellingson@meijer.com>
2024-01-05 09:37:53 -0600

    Fixing some issues with detecting a user in the point

--- src/main/java/com/vanitycraft/VanityControlPoints/VanityControlPoints.java
+++ src/main/java/com/vanitycraft/VanityControlPoints/VanityControlPoints.java
@@ -36,233 +36,239 @@ import com.vanitycraft.VanityControlPoints.Models.Prize;
 import de.tr7zw.nbtapi.NBTItem;
 
 public class VanityControlPoints extends JavaPlugin {
-  public static VanityControlPoints PLUGIN;
+	public static VanityControlPoints PLUGIN;
 
-  public static List<Point> POINTS_ON_COOLDOWN = new ArrayList<Point>();
-  public static List<Point> POINTS = new ArrayList<Point>();
-  public static List<Player> COOLDOWN_NOTIFIED = new ArrayList<Player>();
-  public static List<Prize> PRIZES = new ArrayList<Prize>();
+	public static List<Point> POINTS_ON_COOLDOWN = new ArrayList<Point>();
+	public static List<Point> POINTS = new ArrayList<Point>();
+	public static List<Player> COOLDOWN_NOTIFIED = new ArrayList<Player>();
+	public static List<Prize> PRIZES = new ArrayList<Prize>();
 
-  public static HashMap<Point, List<Player>> POINTS_IN_CONTENTION = new HashMap<Point, List<Player>>();
-  public static HashMap<Point, Player> POINTS_IN_CAPTURE = new HashMap<Point, Player>();
-  public static HashMap<Point, Player> LAST_TO_CONTROL = new HashMap<Point, Player>();
+	public static HashMap<Point, List<Player>> POINTS_IN_CONTENTION = new HashMap<Point, List<Player>>();
+	public static HashMap<Point, Player> POINTS_IN_CAPTURE = new HashMap<Point, Player>();
+	public static HashMap<Point, Player> LAST_TO_CONTROL = new HashMap<Point, Player>();
 
-  public static int COOLDOWN_TIME = 10;
-  public static int CAPTURE_TIME = 5;
+	public static int COOLDOWN_TIME = 10;
+	public static int CAPTURE_TIME = 5;
 
-  @Override
-  public void onEnable() {
-    PLUGIN = this;
+	@Override
+	public void onEnable() {
+		PLUGIN = this;
 
-    setupConfigFiles();
-    loadPointsFromFile();
-    setupTimers();
-    setupPrizes();
+		setupConfigFiles();
+		loadPointsFromFile();
+		setupTimers();
+		setupPrizes();
 
-    // Register event listeners
-    Bukkit.getServer().getPluginManager().registerEvents(new StartCaptureListener(), this);
-    Bukkit.getServer().getPluginManager().registerEvents(new CaptureListener(), this);
-    Bukkit.getServer().getPluginManager().registerEvents(new LeavePointListener(), this);
-    Bukkit.getServer().getPluginManager().registerEvents(new PointContendListener(), this);
+		// Register event listeners
+		Bukkit.getServer().getPluginManager().registerEvents(new StartCaptureListener(), this);
+		Bukkit.getServer().getPluginManager().registerEvents(new CaptureListener(), this);
+		Bukkit.getServer().getPluginManager().registerEvents(new LeavePointListener(), this);
+		Bukkit.getServer().getPluginManager().registerEvents(new PointContendListener(), this);
 
-    // Register commands
-    getCommand("point").setExecutor(new CommandDispatcher());
+		// Register commands
+		getCommand("point").setExecutor(new CommandDispatcher());
 
-    checkPoints();
-  }
+		checkPoints();
+	}
 
-  public void loadPointsFromFile() {
-    File file = new File(getDataFolder(), "points.yml");
+	public void loadPointsFromFile() {
+		File file = new File(getDataFolder(), "points.yml");
 
-    YamlConfiguration points = YamlConfiguration.loadConfiguration(file);
+		YamlConfiguration points = YamlConfiguration.loadConfiguration(file);
 
-    for (String point : points.getConfigurationSection("Points").getKeys(false)) {
-      ConfigurationSection sec = points.getConfigurationSection("Points." + point);
+		for (String point : points.getConfigurationSection("Points").getKeys(false)) {
+			ConfigurationSection sec = points.getConfigurationSection("Points." + point);
 
-      String name = point;
-      String world = sec.getString("World");
+			String name = point;
+			String world = sec.getString("World");
 
-      int pos1X = sec.getInt("pos1.X");
-      int pos1Y = sec.getInt("pos1.Y");
-      int pos1Z = sec.getInt("pos1.Z");
+			int pos1X = sec.getInt("pos1.X");
+			int pos1Y = sec.getInt("pos1.Y");
+			int pos1Z = sec.getInt("pos1.Z");
 
-      int pos2X = sec.getInt("pos2.X");
-      int pos2Y = sec.getInt("pos2.Y");
-      int pos2Z = sec.getInt("pos2.Z");
+			int pos2X = sec.getInt("pos2.X");
+			int pos2Y = sec.getInt("pos2.Y");
+			int pos2Z = sec.getInt("pos2.Z");
 
-      Location pos1 = new Location(Bukkit.getWorld(world), pos1X, pos1Y, pos1Z);
-      Location pos2 = new Location(Bukkit.getWorld(world), pos2X, pos2Y, pos2Z);
+			Location pos1 = new Location(Bukkit.getWorld(world), pos1X, pos1Y, pos1Z);
+			Location pos2 = new Location(Bukkit.getWorld(world), pos2X, pos2Y, pos2Z);
 
-      Point newPoint = new Point(name, world, pos1, pos2);
+			Point newPoint = new Point(name, world, pos1, pos2);
 
-      POINTS.add(newPoint);
-    }
-  }
+			POINTS.add(newPoint);
+		}
+	}
 
-  public void setupTimers() {
-    COOLDOWN_TIME = getConfig().getInt("Point-Cooldown");
-    CAPTURE_TIME = getConfig().getInt("Capture-Time");
-  }
+	public void setupTimers() {
+		COOLDOWN_TIME = getConfig().getInt("Point-Cooldown");
+		CAPTURE_TIME = getConfig().getInt("Capture-Time");
+	}
 
-  public void setupPrizes() {
-    for (String prize : getConfig().getConfigurationSection("Prizes").getKeys(false)) {
-      ConfigurationSection sec = getConfig().getConfigurationSection("Prizes." + prize);
+	public void setupPrizes() {
+		for (String prize : getConfig().getConfigurationSection("Prizes").getKeys(false)) {
+			ConfigurationSection sec = getConfig().getConfigurationSection("Prizes." + prize);
 
-      String itemName = sec.getString("Name");
-      Material itemMaterial = Material.getMaterial(sec.getString("Item"));
-      int itemAmount = sec.getInt("Amount");
-      float chance = Float.parseFloat(sec.getString("Chance"));
+			String itemName = sec.getString("Name");
+			Material itemMaterial = Material.getMaterial(sec.getString("Item"));
+			int itemAmount = sec.getInt("Amount");
+			float chance = Float.parseFloat(sec.getString("Chance"));
 
-      ItemStack newPrize = new ItemStack(itemMaterial, itemAmount);
-      ItemMeta prizeMeta = newPrize.getItemMeta();
+			ItemStack newPrize = new ItemStack(itemMaterial, itemAmount);
+			ItemMeta prizeMeta = newPrize.getItemMeta();
 
-      if(sec.isSet("Name")) {
-        prizeMeta.setDisplayName(ChatColor.translateAlternateColorCodes('&', itemName));
-      }
+			if (sec.isSet("Name")) {
+				prizeMeta.setDisplayName(ChatColor.translateAlternateColorCodes('&', itemName));
+			}
 
-      if (!sec.getStringList("Lore").isEmpty()) {
-        List<String> newLore = new ArrayList<String>();
-        for (String loreLine : sec.getStringList("Lore")) {
-          newLore.add(ChatColor.translateAlternateColorCodes('&', loreLine));
-        }
-        
-        prizeMeta.setLore(newLore);
-      }
+			if (!sec.getStringList("Lore").isEmpty()) {
+				List<String> newLore = new ArrayList<String>();
+				for (String loreLine : sec.getStringList("Lore")) {
+					newLore.add(ChatColor.translateAlternateColorCodes('&', loreLine));
+				}
 
-      if (!sec.getStringList("Echantments").isEmpty()) {
-        for (String enchant : sec.getStringList("Enchantments")) {
-          String[] splitEnchant = enchant.split(",");
+				prizeMeta.setLore(newLore);
+			}
 
-          String enchantName = splitEnchant[0];
-          int enchantLevel = Integer.parseInt(splitEnchant[1]);
+			if (!sec.getStringList("Echantments").isEmpty()) {
+				for (String enchant : sec.getStringList("Enchantments")) {
+					String[] splitEnchant = enchant.split(",");
 
-          prizeMeta.addEnchant(Enchantment.getByKey(NamespacedKey.minecraft(enchantName)), enchantLevel, true);
-        }
-      }
-      
-      newPrize.setItemMeta(prizeMeta);
-      
-      if(sec.isSet("Voucher")) {
-        NBTItem nbtItem = new NBTItem(newPrize);
-        
-        nbtItem.setString("voucher", sec.getString("Voucher"));
-        
-        newPrize = nbtItem.getItem();
-      }
-      
-      Prize prizeObj = new Prize(prize, chance, newPrize);
-      
-      PRIZES.add(prizeObj);
-    }
-  }
+					String enchantName = splitEnchant[0];
+					int enchantLevel = Integer.parseInt(splitEnchant[1]);
 
-  public void setupConfigFiles() {
-    if (!getDataFolder().exists()) {
-      File file = new File(getDataFolder(), "points.yml");
+					prizeMeta.addEnchant(Enchantment.getByKey(NamespacedKey.minecraft(enchantName)), enchantLevel,
+							true);
+				}
+			}
 
-      try {
-        YamlConfiguration points = YamlConfiguration.loadConfiguration(file);
+			newPrize.setItemMeta(prizeMeta);
 
-        points.createSection("Points");
+			if (sec.isSet("Voucher")) {
+				NBTItem nbtItem = new NBTItem(newPrize);
 
-        points.save(file);
+				nbtItem.setString("voucher", sec.getString("Voucher"));
 
-        getConfig().options().copyDefaults(true);
-        saveConfig();
-      } catch (IOException e) {
-        Bukkit.getLogger().log(Level.SEVERE, "There was a problem creating points.yml");
-      }
-    } else {
-      File points = new File(getDataFolder(), "points.yml");
-      File config = new File(getDataFolder(), "config.yml");
+				newPrize = nbtItem.getItem();
+			}
 
-      if (!points.exists()) {
-        try {
-          YamlConfiguration pointsConfig = YamlConfiguration.loadConfiguration(points);
+			Prize prizeObj = new Prize(prize, chance, newPrize);
 
-          pointsConfig.createSection("Points");
+			PRIZES.add(prizeObj);
+		}
+	}
 
-          pointsConfig.save(points);
-        } catch (IOException e) {
-          Bukkit.getLogger().log(Level.SEVERE, "There was a problem creating points.yml");
-        }
-      }
+	public void setupConfigFiles() {
+		if (!getDataFolder().exists()) {
+			File file = new File(getDataFolder(), "points.yml");
 
-      if (!config.exists()) {
-        getConfig().options().copyDefaults(true);
-        saveConfig();
-      }
-    }
-  }
+			try {
+				YamlConfiguration points = YamlConfiguration.loadConfiguration(file);
 
-  private void checkPoints() {
-    new BukkitRunnable() {
-      public void run() {
-        for (Player player : Bukkit.getOnlinePlayers()) {
-          Location pLoc = player.getLocation();
+				points.createSection("Points");
 
-          for (Point point : POINTS) {
-            Vector pVec = pLoc.toVector();
+				points.save(file);
 
-            if (pVec.isInAABB(point.getPosition1().toVector(), point.getPosition2().toVector())
-                && !POINTS_ON_COOLDOWN.contains(point)) {
-              if (!POINTS_IN_CAPTURE.containsKey(point) && !POINTS_IN_CONTENTION.containsKey(point)) {
-                PlayerStartCaptureEvent e = new PlayerStartCaptureEvent(point, player);
+				getConfig().options().copyDefaults(true);
+				saveConfig();
+			} catch (IOException e) {
+				Bukkit.getLogger().log(Level.SEVERE, "There was a problem creating points.yml");
+			}
+		} else {
+			File points = new File(getDataFolder(), "points.yml");
+			File config = new File(getDataFolder(), "config.yml");
 
-                Bukkit.getPluginManager().callEvent(e);
-              } else if (POINTS_IN_CAPTURE.containsKey(point) && POINTS_IN_CAPTURE.get(point) == player
-                  && LeavePointListener.GRACE.containsKey(player)) {
-                LeavePointListener.GRACE.get(player).cancel();
+			if (!points.exists()) {
+				try {
+					YamlConfiguration pointsConfig = YamlConfiguration.loadConfiguration(points);
 
-                LeavePointListener.GRACE.remove(player);
-              } else if (POINTS_IN_CAPTURE.containsKey(point) && !POINTS_IN_CONTENTION.containsKey(point)) {
-                Player capturer = POINTS_IN_CAPTURE.get(point);
+					pointsConfig.createSection("Points");
 
-                if (capturer != player) {
+					pointsConfig.save(points);
+				} catch (IOException e) {
+					Bukkit.getLogger().log(Level.SEVERE, "There was a problem creating points.yml");
+				}
+			}
 
-                  PlayerContendPointEvent e = new PlayerContendPointEvent(player, point);
+			if (!config.exists()) {
+				getConfig().options().copyDefaults(true);
+				saveConfig();
+			}
+		}
+	}
 
-                  Bukkit.getPluginManager().callEvent(e);
-                }
-              }
-            }
+	private void checkPoints() {
+		new BukkitRunnable() {
+			public void run() {
+				for (Player player : Bukkit.getOnlinePlayers()) {
+					Location pLoc = player.getLocation();
 
-            if (pVec.isInAABB(point.getPosition1().toVector(), point.getPosition2().toVector())
-                && POINTS_ON_COOLDOWN.contains(point) && !COOLDOWN_NOTIFIED.contains(player)) {
-              COOLDOWN_NOTIFIED.add(player);
+					for (Point point : POINTS) {
+						Vector pVec = pLoc.toVector();
+						Vector position1 = new Vector(Math.min(point.getPosition1().getX(), point.getPosition2().getX()), Math.min(point.getPosition1().getY(), point.getPosition2().getY()), Math.min(point.getPosition1().getZ(), point.getPosition2().getZ()));
+						Vector position2 = new Vector(Math.max(point.getPosition1().getX(), point.getPosition2().getX()), Math.max(point.getPosition1().getY(), point.getPosition2().getY()), Math.max(point.getPosition1().getZ(), point.getPosition2().getZ()));
 
-              player.sendMessage(
-                  "[" + ChatColor.RED + "ControlPoint" + ChatColor.RESET + "]: This point is currently on cooldown.");
+						if (pVec.isInAABB(position1, position2)
+								&& !POINTS_ON_COOLDOWN.contains(point)) {
+							if (!POINTS_IN_CAPTURE.containsKey(point) && !POINTS_IN_CONTENTION.containsKey(point)) {
+								PlayerStartCaptureEvent e = new PlayerStartCaptureEvent(point, player);
 
-              new BukkitRunnable() {
-                public void run() {
-                  COOLDOWN_NOTIFIED.remove(player);
-                }
-              }.runTaskLater(PLUGIN, (1 * 60) * 20);
-            }
+								Bukkit.getPluginManager().callEvent(e);
+							} else if (POINTS_IN_CAPTURE.containsKey(point) && POINTS_IN_CAPTURE.get(point) == player
+									&& LeavePointListener.GRACE.containsKey(player)) {
+								LeavePointListener.GRACE.get(player).cancel();
 
-            if (!pVec.isInAABB(point.getPosition1().toVector(), point.getPosition2().toVector())) {
-              if (POINTS_IN_CAPTURE.containsKey(point) && POINTS_IN_CAPTURE.get(point) == player
-                  && !LeavePointListener.GRACE.containsKey(POINTS_IN_CAPTURE.get(point))
-                  && !LeavePointListener.GRACE.containsKey(player) && !LAST_TO_CONTROL.containsKey(point)) {
-                PlayerLeavePointEvent e = new PlayerLeavePointEvent(player, point);
+								LeavePointListener.GRACE.remove(player);
+							} else if (POINTS_IN_CAPTURE.containsKey(point)
+									&& !POINTS_IN_CONTENTION.containsKey(point)) {
+								Player capturer = POINTS_IN_CAPTURE.get(point);
 
-                Bukkit.getPluginManager().callEvent(e);
-              } else if (POINTS_IN_CONTENTION.containsKey(point)
-                  && !LeavePointListener.GRACE.containsKey(POINTS_IN_CAPTURE.get(point))
-                  && !LeavePointListener.GRACE.containsKey(player) && !LAST_TO_CONTROL.containsKey(point)) {
-                List<Player> contendingPlayers = POINTS_IN_CONTENTION.get(point);
+								if (capturer != player) {
 
-                if (contendingPlayers.contains(player)) {
-                  PlayerLeavePointEvent e = new PlayerLeavePointEvent(player, point);
+									PlayerContendPointEvent e = new PlayerContendPointEvent(player, point);
 
-                  Bukkit.getPluginManager().callEvent(e);
-                }
-              }
-            }
-          }
-        }
-      }
-    }.runTaskTimer(PLUGIN, 0, 3);
-  }
+									Bukkit.getPluginManager().callEvent(e);
+								}
+							}
+						}
+
+						if (pVec.isInAABB(position1, position2)
+								&& POINTS_ON_COOLDOWN.contains(point) && !COOLDOWN_NOTIFIED.contains(player)) {
+							COOLDOWN_NOTIFIED.add(player);
+
+							player.sendMessage("[" + ChatColor.RED + "ControlPoint" + ChatColor.RESET
+									+ "]: This point is currently on cooldown.");
+
+							new BukkitRunnable() {
+								public void run() {
+									COOLDOWN_NOTIFIED.remove(player);
+								}
+							}.runTaskLater(PLUGIN, (1 * 60) * 20);
+						}
+
+						if (!pVec.isInAABB(position1, position2)) {
+							if (POINTS_IN_CAPTURE.containsKey(point) && POINTS_IN_CAPTURE.get(point) == player
+									&& !LeavePointListener.GRACE.containsKey(POINTS_IN_CAPTURE.get(point))
+									&& !LeavePointListener.GRACE.containsKey(player)
+									&& !LAST_TO_CONTROL.containsKey(point)) {
+								PlayerLeavePointEvent e = new PlayerLeavePointEvent(player, point);
+
+								Bukkit.getPluginManager().callEvent(e);
+							} else if (POINTS_IN_CONTENTION.containsKey(point)
+									&& !LeavePointListener.GRACE.containsKey(POINTS_IN_CAPTURE.get(point))
+									&& !LeavePointListener.GRACE.containsKey(player)
+									&& !LAST_TO_CONTROL.containsKey(point)) {
+								List<Player> contendingPlayers = POINTS_IN_CONTENTION.get(point);
+
+								if (contendingPlayers.contains(player)) {
+									PlayerLeavePointEvent e = new PlayerLeavePointEvent(player, point);
+
+									Bukkit.getPluginManager().callEvent(e);
+								}
+							}
+						}
+					}
+				}
+			}
+		}.runTaskTimer(PLUGIN, 0, 3);
+	}
 }
